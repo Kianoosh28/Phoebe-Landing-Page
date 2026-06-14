46470a0f5edf75f8aaf168be953104b78fe8eacf
Kianoosh28 <Kianoosh28@users.noreply.github.com>
2026-06-14 21:44:48 +0200

    Recolor VivaTech 'VIVA' text to white; bump cache-buster to v6

--- src/app/flyer-preview/page.tsx
+++ src/app/flyer-preview/page.tsx
@@ -216,7 +216,7 @@ function SideA({ logoSvg }: { logoSvg: string }) {
           <div className="flex flex-1 items-center justify-center" style={{ padding: "0 4mm" }}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src="/vivatech-logo.svg?v=4"
+              src="/vivatech-logo.svg?v=6"
               alt="VivaTech"
               style={{ maxHeight: "96px", maxWidth: "100%", width: "auto" }}
             />
